26723cdd458326bc7527f80f3402f09d27fa5375
vabegomes <vabegomes@gmail.com>
2022-05-02 23:39:27 +0000

    traffic light

---
+++ src/js/component/Home.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useState } from "react";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+export default function Home() {
+	const [selectedColor, setSelectedColor] = useState("yellow");
+	//let selectedColor="yellow";
+
+	return (
+		<div className="traffic-light">
+			<div
+				onClick={() => setSelectedColor("red")}
+				className={
+					"light red" + (selectedColor === "red" ? " glow" : "")
+				}></div>
+			<div
+				onClick={() => setSelectedColor("yellow")}
+				className={
+					"light yellow" + (selectedColor === "yellow" ? " glow" : "")
+				}></div>
+			<div
+				onClick={() => setSelectedColor("green")}
+				className={
+					"light green" + (selectedColor === "green" ? " glow" : "")
+				}></div>
+		</div>
+	);
+}
